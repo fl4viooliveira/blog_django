10e9bbed979cddd253a1bce496ff498f2c439941
fl4viooliveira <fl4viooliveira@gmail.com>
2020-08-09 13:10:34 +0100

    custom user

--- posts/models.py
+++ posts/models.py
@@ -1,4 +1,13 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+    pass
+
+    def __str__(self):
+        return self.username
+
 
 
 class Post(models.Model):
@@ -7,13 +16,13 @@ class Post(models.Model):
     thumbnail = models.ImageField()
     publish_date = models.DateTimeField(auto_now_add=True)
     last_update = models.DateTimeField(auto_now=True)
-    # author = models.ForeignKey()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
 
 class Comment(models.Model):
-    # user = models.ForeignKey()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     content = models.TextField()
@@ -23,7 +32,7 @@ class Comment(models.Model):
 
 
 class PostView(models.Model):
-    # user = models.ForeignKey()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
     timestamp = models.DateTimeField(auto_now_add=True)
     
@@ -32,7 +41,7 @@ class PostView(models.Model):
 
 
 class Like(models.Model):
-    # user = models.ForeignKey()
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
    
     def __str__(self):
